814b595e42791af35c0c4baf9c34672e42085cd4
Marcel Bankmann <marcel.bankmann@devbliss.com>
2015-01-12 17:34:45 +0100

    created first task and switched to wiredep

--- tasks/devbliss.js
+++ tasks/devbliss.js
@@ -8,62 +8,58 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+    var wireDep = require('grunt-wiredep/tasks/wiredep.js')(grunt);
 
-  //require('load-grunt-tasks')(grunt,{pattern: ['!grunt-devbliss']});
-  //grunt.loadNpmTasks('grunt-bower-install');
-  //var bowerConfig = require('bower-config');
-  var helper = require('grunt-lib-contrib').init(grunt);
- //var bowerInstall = require('grunt-bower-install').init(grunt);
-  grunt.initConfig({
-    bowerInstall: {
-      app: {
-        src: ['app/index.html']
-      }
-    }
-  });
-  grunt.registerTask('devbliss-bower-install', function() {
-    //grunt.loadNpmTasks('grunt-bower-install');
-    //grunt.task.run(['bowerInstall']);
-        grunt.log.warn('devbliss bower install implementation');
-  }
-  );
-
-  grunt.registerMultiTask('devbliss', 'The best Grunt plugin ever.', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
-
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
+    grunt.registerTask('devbliss-bower-install', function () {
+            grunt.config.merge({
+                wiredep: {
+                    task: {
+                        // Point to the files that should be updated when
+                        // you run `grunt wiredep`
+                        src: [
+                            'app/index.html'
+                        ]
+                    }
+                }
+            });
+            grunt.task.run(['wiredep']);
         }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
+    );
 
-      // Handle options.
-      src += options.punctuation;
+    grunt.registerMultiTask('devbliss', 'The best Grunt plugin ever.', function () {
+        // Merge task-specific and/or target-specific options with these defaults.
+        var options = this.options({
+            punctuation: '.',
+            separator: ', '
+        });
 
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
+        // Iterate over all specified file groups.
+        this.files.forEach(function (f) {
+            // Concat specified files.
+            var src = f.src.filter(function (filepath) {
+                // Warn on and remove invalid source files (if nonull was set).
+                if (!grunt.file.exists(filepath)) {
+                    grunt.log.warn('Source file "' + filepath + '" not found.');
+                    return false;
+                } else {
+                    return true;
+                }
+            }).map(function (filepath) {
+                // Read file source.
+                return grunt.file.read(filepath);
+            }).join(grunt.util.normalizelf(options.separator));
 
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
+            // Handle options.
+            src += options.punctuation;
+
+            // Write the destination file.
+            grunt.file.write(f.dest, src);
+
+            // Print a success message.
+            grunt.log.writeln('File "' + f.dest + '" created.');
+        });
     });
-  });
 
 };
