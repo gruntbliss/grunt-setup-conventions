940a59e2591e74008f2570dd335330758b36657f
Bastien Mary <bastien.mary@devbliss.com>
2015-01-28 18:56:20 +0100

    add htmlmin task

---
+++ tasks/htmlmin.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    // External Dependencies import
+    require('grunt-contrib-htmlmin/tasks/htmlmin.js')(grunt);
+
+    function loadConfig(grunt) {
+        // MINIFI TEMP HTML AND MOVE IT TO DIST FOLDER
+        return {
+            dist: {
+                options: {
+                    collapseWhitespace: true,
+                        collapseBooleanAttributes: true,
+                        removeCommentsFromCDATA: true,
+                        removeOptionalTags: true
+                },
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'dist',
+                        src: ['**/*.html'],
+                        dest: 'dist'
+                    }
+                ]
+            }
+        }
+
+    }
+
+    grunt.registerTask('devbliss-htmlmin', function () {
+            grunt.config.merge(loadConfig(grunt));
+            grunt.task.run(['htmlmin']);
+        }
+    );
+};
