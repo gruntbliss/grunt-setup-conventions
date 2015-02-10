5ba40eb3e881df83bbfae66138cd2f1e086d8c19
Michael Diodone <michael.diodone@devbliss.com>
2015-02-10 11:00:18 +0100

    Added possibility to replace proxy config

--- tasks/configureProxies.js
+++ tasks/configureProxies.js
@@ -6,28 +6,36 @@ module.exports = function (grunt) {
     require('grunt-connect-proxy/tasks/connect_proxy.js')(grunt);
 
     function loadConfig(grunt) {
+        // configuration for task configureProxies from plugin grunt-connect-proxy
+        var proxies = [
+            { // used for requests to the zuul service in local development
+                context: '/api',
+                host: '172.17.42.1', // ip address of the host
+                port: 8070,
+                changeOrigin: true,
+                xforward: false
+            },
+            { // used for content player in local development
+                context: '/qtiplayer',
+                host: 'localhost',
+                port: 13771,
+                changeOrigin: true,
+                xforward: false,
+                rewrite: {
+                    '^/qtiplayer': ''
+                }
+            }
+        ];
+        // get existing options to overwrite the task rules with the project rules
+        var projectProxies = grunt.config('connect.proxies');
+
+        if (Array.isArray(projectProxies)) {
+            proxies = projectProxies;
+        }
+
         return {
             connect: {
-                // configuration for task configureProxies from plugin grunt-connect-proxy
-                proxies: [
-                    { // used for requests to the zuul service in local developement
-                        context: '/api',
-                        host: '172.17.42.1', // ip address of the host
-                        port: 8070,
-                        changeOrigin: true,
-                        xforward: false
-                    },
-                    { // used for content player in local developement
-                        context: '/qtiplayer',
-                        host: 'localhost',
-                        port: 13771,
-                        changeOrigin: true,
-                        xforward: false,
-                        rewrite: {
-                            '^/qtiplayer': ''
-                        }
-                    }
-                ]
+                proxies: proxies
             }
         };
     }
