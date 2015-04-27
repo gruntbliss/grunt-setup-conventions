007bae5ce645d58e028d2eee424c64a97c214315
Bastien Mary <bastien.mary@devbliss.com>
2015-04-27 09:21:31 +0200

    copy task is now implemented with extend-grunt-plugin

--- tasks/copy.js
+++ tasks/copy.js
@@ -1,94 +1,83 @@
 'use strict';
 
+var extendGruntPlugin = require('extend-grunt-plugin');
+
 module.exports = function (grunt) {
 
     var helpers = require('./util/helpers.js');
 
-    // External Dependencies import
-    require('grunt-contrib-copy/tasks/copy.js')(grunt);
-
-    function loadConfig() {
-        return {
-            // COPYS FILES FROM APP TO DIST
-            copy: {
-                components: {
-                    files: [
-                        {
-                            expand: true,
-                            dot: true,
-                            cwd: 'app',
-                            dest: 'dist',
-                            src: [
-                                '.htaccess',
-                                'bower_components/**/*.html',
-                                'app_components/**/*.html',
-                                'views/**/*.html',
-                                'index.html'
-                            ]
-                        }
-                    ]
-                },
-                js: {
-                    files: [
-                        {
-                            expand: true,
-                            dot: true,
-                            cwd: '.tmp/concat/scripts/',
-                            dest: 'dist/scripts/',
-                            src: ['*.js']
-                        }
-                    ]
-                },
-                font: {
-                    files: [
-                        {
-                            expand: true,
-                            dot: true,
-                            cwd: 'app',
-                            src: [
-                                'styles/css/bootstrap/fonts/**/*',
-                                'styles/fonts/**/*',
-                                'bower_components/ecosystem-main-frontend/release/styles/bootstrap/fonts/**/*'
-                            ],
-                            dest: 'dist'
-                        }
-                    ]
-                },
-                img: {
-                    files: [
-                        {
-                            expand: true,
-                            dot: true,
-                            cwd: 'app',
-                            src: [
-                                'bower_components/ecosystem-main-frontend/release/images/**/*',
-                                'images/placeholder/*',
-                                'styles/img/**/*'
-                            ],
-                            dest: 'dist'
-                        }
-                    ]
-                },
-                release: {
-                    files: [
-                        {
-                            expand: true,
-                            dot: true,
-                            cwd: 'app/app_components/pageModule/',
-                            dest: 'release/',
-                            src: [
-                                '**/*'
-                            ]
-                        }
+    var taskConfig = {
+        components: {
+            files: [
+                {
+                    expand: true,
+                    dot: true,
+                    cwd: 'app',
+                    dest: 'dist',
+                    src: [
+                        '.htaccess',
+                        'bower_components/**/*.html',
+                        'app_components/**/*.html',
+                        'views/**/*.html',
+                        'index.html'
                     ]
                 }
-            }
-        };
-    }
-
-    grunt.registerTask('devbliss-copy', function (config) {
-            grunt.config.merge(loadConfig());
-            grunt.task.run(['copy' + helpers.gruntTarget(config)]);
+            ]
+        },
+        js: {
+            files: [
+                {
+                    expand: true,
+                    dot: true,
+                    cwd: '.tmp/concat/scripts/',
+                    dest: 'dist/scripts/',
+                    src: ['*.js']
+                }
+            ]
+        },
+        font: {
+            files: [
+                {
+                    expand: true,
+                    dot: true,
+                    cwd: 'app',
+                    src: [
+                        'styles/css/bootstrap/fonts/**/*',
+                        'styles/fonts/**/*',
+                        'bower_components/ecosystem-main-frontend/release/styles/bootstrap/fonts/**/*'
+                    ],
+                    dest: 'dist'
+                }
+            ]
+        },
+        img: {
+            files: [
+                {
+                    expand: true,
+                    dot: true,
+                    cwd: 'app',
+                    src: [
+                        'bower_components/ecosystem-main-frontend/release/images/**/*',
+                        'images/placeholder/*',
+                        'styles/img/**/*'
+                    ],
+                    dest: 'dist'
+                }
+            ]
         }
-    );
+    };
+
+
+    grunt.registerTask('devbliss-copy', function(config) {
+
+        extendGruntPlugin(grunt, require('grunt-contrib-copy/tasks/copy'), {
+            'copy.components': taskConfig.components,
+            'copy.js': taskConfig.js,
+            'copy.font': taskConfig.font,
+            'copy.img': taskConfig.img
+        });
+
+        grunt.task.run(['copy' + helpers.gruntTarget(config)]);
+    });
 };
+
